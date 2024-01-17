f5740a26ef996b41cac7b0231b2a4f256c02c0f8
talafarael <127351345+talafarael@users.noreply.github.com>
2024-01-17 15:17:26 +0200

    гкд

--- regist.js
+++ regist.js
@@ -28,7 +28,7 @@ button.addEventListener('click', async (event) => {
       if (response.ok) {
          // Redirection to the 'sendemail' route on successful registration
          const baseUrl = window.location.origin
-      window.location.href = `${baseUrl}/project_frontend/code.html`;
+      window.location.href = `${baseUrl}/code.html`;
         console.log(baseUrl);
          
       }
